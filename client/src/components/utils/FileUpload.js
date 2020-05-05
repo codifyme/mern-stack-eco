cdd51fd3ffd9f22aaf3ad319accc91e5f37a365b
Angel Cochachez <acochachez@gmail.com>
2020-05-05 02:43:09 -0700

    fixing product modal and complete uploading product page

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,78 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
-import {Icon} from 'antd';
+import { Icon } from 'antd';
 import Axios from 'axios';
 // import {response} from 'express';
 
-
 function FileUpload(props) {
+	const [ Images, setImages ] = useState([]);
 
-    const [Images, setImages]=useState([])
+	const onDrop = (files) => {
+		let formData = new FormData();
+		const config = {
+			header: { 'content-type': 'multipart/form-data' }
+		};
+		formData.append('file', files[0]);
 
-    const onDrop=(files)=>{
+		//save the image we chose inside the Node Server
+		Axios.post('/api/product/uploadImage', formData, config).then((response) => {
+			if (response.data.success) {
+				setImages([ ...Images, response.data.image ]);
+				props.refreshFunction([ ...Images, response.data.image ]);
+			} else {
+				alert('Failed to save the Image in Server');
+			}
+		});
+	};
 
-        let formData = new FormData();
-        const config = {
-            header:{'content-type': 'multipart/form-data'}
-        }
-        formData.append("file", files[0])
+	const onDelete = (image) => {
+		const currentIndex = Images.indexOf(image);
 
-        //save the image we chose inside the Node Server
-        Axios.post('/api/product/uploadImage', formData, config)
-            .then(response => {
-                if(response.data.success){
-                    setImages([...Images, response.data.image])
-                    props.refreshFunction([...Images, response.data.image])
+		let newImages = [ ...Images ];
+		newImages.spide(currentIndex, 1);
 
-                }else{
-                     alert('Failed to save the Image in Server')
-                }
-            })
-    }
+		setImages(newImages);
+	};
 
-    const onDelete=(image)=>{
-        const currentIndex=Images.indexOf(image);
-
-        let newImages=[...Images]
-        newImages.spide(currentIndex,1)
-
-        setImages(newImages)
-
-
-    }
-
-    return (
-        <div style={{display:'flex', justifyContent:'space-between'}}>
-            <Dropzone
-                onDrop={onDrop}
-                multiple={false}
-                maxSize={800000000}
-            >
-                {({getRootProps, getInputProps}) => (
-                    <div style={{
-                        width:'300px', height:'240px', border:'1px solid lightgray',
-                        display:'flex', alignItems:'center', justifyContent:'center' 
-                    }}
-                        {...getRootProps()}
-                    >
-                        <input {...getInputProps()}/>
-                        <Icon type="plus" style={{ fontSize:'3rem'}} />
-
-                    </div>
-                )}
-            </Dropzone>
-                    <div style={{display:'flex', width:'350px', height:'240px', overflowX:'scroll'}}>
-                        {Images.map((image, index) => (
-                            <div onClick>
-                                <img style={{minWidth:'300px', width:'300px', height:'240px'}} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
-                        </div>
-                        ))}
-                    </div>
-                
-
-
-        </div>
-    )
+	return (
+		<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+			<Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+				{({ getRootProps, getInputProps }) => (
+					<div
+						style={{
+							width: '300px',
+							height: '240px',
+							border: '1px solid lightgray',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center'
+						}}
+						{...getRootProps()}
+					>
+						<input {...getInputProps()} />
+						<Icon type='plus' style={{ fontSize: '3rem' }} />
+					</div>
+				)}
+			</Dropzone>
+			<div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
+				{Images.map((image, index) => (
+					<div onClick>
+						<img
+							style={{ minWidth: '300px', width: '300px', height: '240px' }}
+							src={`http://localhost:5000/${image}`}
+							alt={`productImg-${index}`}
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default FileUpload;
